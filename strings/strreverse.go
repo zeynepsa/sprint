b4154dc9f19dcbb97c40d82048cb5e790d0f7538
zeynepsa <zeynepsahinavci@gmail.com>
2026-04-06 10:55:28 +0300

    aä

---
+++ strings/strreverse.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	print(StrReverse("Hello Coder!"))
+}
+
+func StrReverse(s string) string {
+	result := ""
+
+	for i:=len(s)-1 ;i>=0 ;i-- {
+
+		result += string(s[i])
+	}
+	return result
+}
